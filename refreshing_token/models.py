c63543b05aedd155094e3e69b000d94a07f8dc49
neel-bp <neel@bitspro.com>
2022-02-10 23:46:53 +0500

    added authentication class and refreshing model

--- refreshing_token/models.py
+++ refreshing_token/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+import binascii
+import os
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class RefreshToken(models.Model):
+    """
+    The default authorization token model.
+    """
+    key = models.CharField(_("Key"), max_length=40, primary_key=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='refresh_token',
+        on_delete=models.CASCADE, verbose_name=_("User")
+    )
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+
+    class Meta:
+        abstract = 'refreshing_token' not in settings.INSTALLED_APPS
+        verbose_name = _("RefreshToken")
+        verbose_name_plural = _("RefreshTokens")
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super().save(*args, **kwargs)
+
+    @classmethod
+    def generate_key(cls):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
+
+
+class TokenProxy(RefreshToken):
+    """
+    Proxy mapping pk to user pk for use in admin.
+    """
+    @property
+    def pk(self):
+        return self.user_id
+
+    class Meta:
+        proxy = 'refreshing_token' in settings.INSTALLED_APPS
+        abstract = 'refreshing_token' not in settings.INSTALLED_APPS
+        verbose_name = "token"
