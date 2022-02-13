c184d972b0d9b41d2048fa2fd36c1b4794290b13
neel-bp <neel@bitspro.com>
2022-02-13 16:18:04 +0500

    changed model definition, now both access token and refresh token inherit from rest framework auth token model

--- refreshing_token/models.py
+++ refreshing_token/models.py
@@ -7,33 +7,16 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework.authtoken.models import Token
 
 
-class RefreshToken(models.Model):
-    """
-    The default authorization token model.
-    """
+class RefreshToken(Token):
     key = models.CharField(_("Key"), max_length=256, primary_key=True)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name='refresh_token',
         on_delete=models.CASCADE, verbose_name=_("User")
     )
-    created = models.DateTimeField(_("Created"), auto_now_add=True)
 
     class Meta:
         verbose_name = _("RefreshToken")
         verbose_name_plural = _("RefreshTokens")
-
-    def save(self, *args, **kwargs):
-        if not self.key:
-            self.key = self.generate_key()
-        return super().save(*args, **kwargs)
-
-    @classmethod
-    def generate_key(cls):
-        return binascii.hexlify(os.urandom(20)).decode()
-
-    def __str__(self):
-        return self.key
-
 
 class RefreshTokenProxy(RefreshToken):
     """
@@ -49,6 +32,10 @@ class RefreshTokenProxy(RefreshToken):
 
 class AccessToken(Token):
     key = models.CharField(_("Key"), max_length=256, primary_key=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='access_token',
+        on_delete=models.CASCADE, verbose_name=_("User")
+    )
 
     class Meta:
         verbose_name = _("AccessToken")
