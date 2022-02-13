3743509f885e15dbd5934ec4d41a79607dcd87c7
neel-bp <neel@bitspro.com>
2022-02-13 16:05:57 +0500

    added quick views for generating pair and refreshing the access token, also one for verifying

--- refreshing_token/views.py
+++ refreshing_token/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from refreshing_token.util import generate_token_pair, refresh_access_token
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
 
-# Create your views here.
+class GenerateTokenPair(APIView):
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username,password=password)
+        if user is None:
+            return Response(data={"error":"no user found with provided username and password"},status=status.HTTP_401_UNAUTHORIZED)
+        
+        token_pair = generate_token_pair(user)
+
+        return Response(data=token_pair)  
+
+class Verify(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        return Response(data={"yep its working"})
+
+class RefreshTokenView(APIView):
+    
+    def post(self,request):
+        refresh_token = request.data.get("refresh_token")
+        access_token = refresh_access_token(refresh_token=refresh_token)
+
+        return Response(data={"access_token":access_token})
+
+
